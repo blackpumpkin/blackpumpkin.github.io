80efbe655e11e77a11c6530b32f3fac5be848e60
blackpumpkin <celebi.phn@gmail.com>
2023-01-08 05:49:16 +0800

    Site updated: 2023-01-08 05:49:16

--- js/runtime.js
+++ js/runtime.js
@@ -1,36 +1,32 @@
+//-页脚网站运行时间等信息
+var now = new Date();
+function createtime() {
+  // 当前时间
+  now.setTime(now.getTime() + 1000);
+  var start = new Date("08/01/2022 00:00:00"); // 旅行者1号开始计算的时间
+  var dis = Math.trunc(23400000000 + ((now - start) / 1000) * 17); // 距离=秒数*速度 记住转换毫秒
+  var unit = (dis / 149600000).toFixed(6);  // 天文单位
+  var grt = new Date("12/17/2022 00:00:00");	// 网站诞生时间
+  var days = (now - grt) / 1e3 / 60 / 60 / 24,
+    dnum = Math.floor(days),
+    hours = (now - grt) / 1e3 / 60 / 60 - 24 * dnum,
+    hnum = Math.floor(hours);
+  1 == String(hnum).length && (hnum = "0" + hnum);
+  var minutes = (now - grt) / 1e3 / 60 - 1440 * dnum - 60 * hnum,
+    mnum = Math.floor(minutes);
+  1 == String(mnum).length && (mnum = "0" + mnum);
+  var seconds = (now - grt) / 1e3 - 86400 * dnum - 3600 * hnum - 60 * mnum,
+    snum = Math.round(seconds);
+  1 == String(snum).length && (snum = "0" + snum);
+  let currentTimeHtml = "";
+  (currentTimeHtml =
+    hnum < 18 && hnum >= 9
+    ? `<img class='boardsign' src='https://img.shields.io/badge/%E5%8D%97%E7%93%9C%E3%81%AE%E5%B0%8F%E5%B1%8B-%E6%91%B8%E9%B1%BC%E4%B8%AD-6adea8?style=social&logo=coffeescript' title='今天也是适合摸鱼的一天~'><br> <div style="font-size:13px;font-weight:bold">本站居然运行了 ${dnum} 天 ${hnum} 小时 ${mnum} 分 ${snum} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> <br> 旅行者 1 号当前距离地球 ${dis} 千米，约为 ${unit} 个天文单位 🚀</div>`
+    : `<img class='boardsign' src='https://img.shields.io/badge/%E5%8D%97%E7%93%9C%E3%81%AE%E5%B0%8F%E5%B1%8B-%E6%89%93%E7%83%8A%E4%BA%86-6adea8?style=social&logo=coffeescript' title='忙碌的一天结束了~'><br> <div style="font-size:13px;font-weight:bold">本站居然运行了 ${dnum} 天 ${hnum} 小时 ${mnum} 分 ${snum} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> <br> 旅行者 1 号当前距离地球 ${dis} 千米，约为 ${unit} 个天文单位 🚀</div>`),
+    document.getElementById("workboard") &&
+    (document.getElementById("workboard").innerHTML = currentTimeHtml);
+}
+// 设置重复执行函数，周期1000ms
 setInterval(() => {
-    let create_time = Math.round(new Date('2022-12-22 00:00:00').getTime() / 1000); //在此行修改建站时间
-    let timestamp = Math.round((new Date().getTime()) / 1000);
-    let second = timestamp - create_time;
-    let time = new Array(0, 0, 0, 0, 0);
-  
-    var nol = function(h){
-      return h>9?h:'0'+h;
-    }
-    if (second >= 365 * 24 * 3600) {
-      time[0] = parseInt(second / (365 * 24 * 3600));
-      second %= 365 * 24 * 3600;
-    }
-    if (second >= 24 * 3600) {
-      time[1] = parseInt(second / (24 * 3600));
-      second %= 24 * 3600;
-    }
-    if (second >= 3600) {
-      time[2] = nol(parseInt(second / 3600));
-      second %= 3600;
-    }
-    if (second >= 60) {
-      time[3] = nol(parseInt(second / 60));
-      second %= 60;
-    }
-    if (second > 0) {
-      time[4] = nol(second);
-    }
-    if ((Number(time[2])<22) && (Number(time[2])>7)){
-      currentTimeHtml ="<img class='boardsign' src='https://img.shields.io/badge/南瓜的小屋-营业中-6adea8?style=social&logo=cakephp' title='今天又是适合努力摸鱼的一天~'><div id='runtime'>" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>';
-    }
-    else{
-      currentTimeHtml ="<img class='boardsign' src='https://img.shields.io/badge/南瓜的小屋-打烊了-6adea8?style=social&logo=coffeescript' title='这个点了应该去睡觉啦，熬夜对身体不好哦'><div id='runtime'>" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>';
-    }
-    document.getElementById("workboard").innerHTML = currentTimeHtml;
-  }, 1000);
+  createtime();
+}, 1000);
