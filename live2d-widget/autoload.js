b7009e36e81f709606e7c5ea5679f84b2348ad13
blackpumpkin <celebi.phn@gmail.com>
2023-02-07 21:58:19 +0800

    Site updated: 2023-02-07 21:58:18

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -36,7 +36,10 @@ if (screen.width >= 768) {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/",
+			//cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/",
+			//apiPath: "https://live2d.fghrsh.net/api/",
+  			//cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
+			cdnPath: "https://npm.elemecdn.com/akilar-live2dapi@latest/"
 			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
 		});
 	});
