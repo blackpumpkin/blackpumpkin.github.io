879d38afae9ca83ef655e4f63ca9cac1bd67eb54
blackpumpkin <celebi.phn@gmail.com>
2023-02-28 11:54:25 +0800

    Site updated: 2023-02-28 11:54:25

--- js/rightmenu.js
+++ js/rightmenu.js
@@ -61,6 +61,24 @@ rmf.showRightMenu = function (isTrue, x = 0, y = 0) {
     }
 }
 
+// 昼夜切换
+rmf.switchDarkMode = function () {
+    const nowMode = document.documentElement.getAttribute('data-theme') === 'dark' ? 'dark' : 'light'
+    if (nowMode === 'light') {
+        activateDarkMode()
+        saveToLocal.set('theme', 'dark', 2)
+        GLOBAL_CONFIG.Snackbar !== undefined && btf.snackbarShow(GLOBAL_CONFIG.Snackbar.day_to_night)
+    } else {
+        activateLightMode()
+        saveToLocal.set('theme', 'light', 2)
+        GLOBAL_CONFIG.Snackbar !== undefined && btf.snackbarShow(GLOBAL_CONFIG.Snackbar.night_to_day)
+    }
+    // handle some cases
+    typeof utterancesTheme === 'function' && utterancesTheme()
+    typeof FB === 'object' && window.loadFBComment()
+    window.DISQUS && document.getElementById('disqus_thread').children.length && setTimeout(() => window.disqusReset(), 200)
+}
+
 rmf.copyWordsLink = function () {
     let url = window.location.href
     let txa = document.createElement("textarea");
