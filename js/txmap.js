b9d33b32f6fedec4f3a5eff966c452ca156751db
blackpumpkin <celebi.phn@gmail.com>
2023-04-09 21:12:16 +0800

    Site updated: 2023-04-09 21:12:15

--- js/txmap.js
+++ js/txmap.js
@@ -30,7 +30,7 @@ function getDistance(e1, n1, e2, n2) {
 
 function showWelcome() {
 
-    let dist = getDistance(40.140701, 117.121383, ipLoacation.result.location.lng, ipLoacation.result.location.lat); //这里换成自己的经纬度
+    let dist = getDistance(117.121383, 40.140701, ipLoacation.result.location.lng, ipLoacation.result.location.lat); //这里换成自己的经纬度
     let pos = ipLoacation.result.ad_info.nation;
     let ip;
     let posdesc;
