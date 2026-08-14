1364c79edce7367e1e426755ffd6f83054156f27
blackpumpkin <celebi.phn@gmail.com>
2026-08-14 23:50:10 +0800

    Site updated: 2026-08-14 23:50:10

--- js/auto-index.js
+++ js/auto-index.js
@@ -1,5 +1,4 @@
 //  字节跳动站长管理工具自动收录
-<script>
 (function(){
 var el = document.createElement("script");
 el.src = "https://lf1-cdn-tos.bytegoofy.com/goofy/ttzz/push.js?350f646e1974f66b772ddf0fa1d765e748b666313f3255750360c2e7987b804831a338bfe8517054addc861a521fc04670aa30cdf5a37e663d5331479b5a12d275476d6d0d2e368be4dbc26e37139219";
@@ -7,4 +6,3 @@ el.id = "ttzz";
 var s = document.getElementsByTagName("script")[0];
 s.parentNode.insertBefore(el, s);
 })(window)
-</script>
